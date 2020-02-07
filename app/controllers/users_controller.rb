060174f373fd807ec31206515f4a297c3f160948
github username <github email address>
2020-02-06 20:14:38 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
 
+    get '/' do
+        erb :index
+    end
+    
+    get '/signup' do 
+        erb :'users/new'
+    end
+
+    post '/signup' do 
+        @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+        redirect "/tweets"
+    end
+
+    get '/login' do 
+        erb :'users/login'
+    end
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user != nil && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+        end
+        # binding.pry
+        if logged_in?
+            redirect "/tweets"
+        else 
+            redirect '/signup'
+        end
+    end
+
+    
+    get '/tweets' do
+        erb :'users/tweets'
+    end
+
+
+    
 
 end
--- config.ru
+++ config.ru
@@ -1,8 +1,11 @@
 require './config/environment'
 
-if ActiveRecord::Base.connection.migration_context.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
-end
+# if ActiveRecord::Base.connection.migration_context.needs_migration?
+#   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
+# end
+
+# designate collaborator, fork and clone, remote repo, locally create own branches - development
+# then push, approve only other person's pull request
 
 use Rack::MethodOverride
 use TweetsController
